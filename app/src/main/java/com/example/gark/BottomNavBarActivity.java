a94a903a9071b615ead4341e3811fc8de2392d47
tahaJemmali <49129221+tahaJemmali@users.noreply.github.com>
2021-01-18 00:13:54 +0100

    footer and header

---
+++ app/src/main/java/com/example/gark/BottomNavBarActivity.java
@@ -0,0 +1,14 @@
+package com.example.gark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomNavBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_bar);
+    }
+}
---
+++ app/src/main/java/com/example/gark/TopBarActivity.java
@@ -0,0 +1,14 @@
+package com.example.gark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_bar);
+    }
+}

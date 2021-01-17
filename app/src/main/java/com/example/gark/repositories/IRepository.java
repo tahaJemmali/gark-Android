8dcb1df23b8a6054dae2a449056b9e94ea06adca
tahaJemmali <49129221+tahaJemmali@users.noreply.github.com>
2021-01-17 19:19:12 +0100

    heroku

--- app/src/main/java/com/example/gark/repositories/IRepository.java
+++ app/src/main/java/com/example/gark/repositories/IRepository.java
@@ -1,7 +1,8 @@
 package com.example.gark.repositories;
 
 public interface IRepository {
-    public static String baseURL = "http://192.168.209.1:3000";
+  //  public static String baseURL = "http://192.168.209.1:3000";
+  public static String baseURL = "https://gark-backend.herokuapp.com";
     void showLoadingButton();
     void doAction();
     void dismissLoadingButton();
